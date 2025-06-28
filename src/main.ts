08d0ab9423d311d6124910ee266298c088de7717
jhonf2004 <jhonf5025@gmail.com>
2025-06-27 22:20:09 -0500

    correcion  estructura backend + frontend

--- src/main.ts
+++
@@ -1,8 +0,0 @@
-// src/main.ts
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-// IMPORTANTE: importar tu CSS con Tailwind
-import './index.css'
-
-createApp(App).use(router).mount('#app')
